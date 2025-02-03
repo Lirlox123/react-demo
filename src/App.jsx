6cd07d28362b158d1323498dee25183d59e42b88
Lirlox123 <lirlox123@gmail.com>
2025-02-02 18:22:28 -0600

    Version Base

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Button from './components/Button'
+
+function App() {
+  const [contador, setContador] = useState(0)
+
+  const IncrementarContador = () => setContador(contador + 1)
+  const DecrementarContador = () => setContador(contador -1 )
+    
+  return (
+    <>
+    <h1 className='text-6xl'>Contador: {contador}</h1>
+    <Button text={'Incrementar'} funcionalidad={IncrementarContador}/>
+    <Button text={'Decrementar'} funcionalidad={DecrementarContador}/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Button({text,funcionalidad})
+{
+    return(
+        <div>
+            <button onClick={() => funcionalidad()}
+                className='bg-blue-900 text-3xl text-white p-6 m-4 rounded-lg'>
+                {text}</button>
+        </div>
+    )
+}
