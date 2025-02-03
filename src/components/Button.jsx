d2204b60a287c5371ee0b93a1c27a74756c3e550
Lirlox123 <lirlox123@gmail.com>
2025-02-02 18:42:19 -0600

    refactor(Button.jsx): Cambiar color de el botón -Cambiar color de fondo del componente a naranja en vez de azul por medio de clases de TailwindCSS

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ export default function Button({text,funcionalidad})
     return(
         <div>
             <button onClick={() => funcionalidad()}
-                className='bg-blue-900 text-3xl text-white p-6 m-4 rounded-lg'>
+                className='bg-orange-500 text-3xl text-white p-6 m-4 rounded-lg'>
                 {text}</button>
         </div>
     )
